80394570329466b65d97cde8e0b77e789c753e60
Nihal <nihal@warewe.com>
2025-10-24 17:07:03 +0530

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import vercel from '@astrojs/vercel';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), sitemap()],
+  adapter: vercel()
+});
